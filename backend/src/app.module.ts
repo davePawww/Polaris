f0c6044cd5fd6d4c9b1a765be8ff06c7ff516c02
DavePaurillo <paurillo.dave@gmail.com>
2025-08-17 02:29:39 +0800

    setup: Prisma global module | Users api

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './modules/users/users.controller';
+import { PrismaModule } from './database/prisma.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
+  imports: [PrismaModule, UsersModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- backend/src/modules/users/users.controller.ts
+++ backend/src/modules/users/users.controller.ts
@@ -1,22 +1,41 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() data: Prisma.UserCreateInput) {
+    return this.usersService.createUser(data);
+  }
+
   @Get()
   findAll() {
-    return {
-      success: true,
-      message: 'Users fetched successfully',
-      data: [],
-    };
+    return this.usersService.users({});
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return {
-      success: true,
-      message: 'User fetched successfully',
-      data: [],
-    };
+    return this.usersService.user({ id });
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() data: Prisma.UserUpdateInput) {
+    return this.usersService.updateUser({ where: { id }, data });
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.usersService.deleteUser({ id });
   }
 }
