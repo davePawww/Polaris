0ca0f1792a16c270a63b410e1252853732128833
DavePaurillo <paurillo.dave@gmail.com>
2025-08-24 00:12:57 +0800

    setup: clerk vue

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/vue'
 import Container from './Container.vue'
 </script>
 
 <template>
   <header class="sticky top-0 z-50 bg-black text-white">
     <Container>
-      <p>This is the header</p>
+      <div class="flex items-center justify-between">
+        <p>This is the header</p>
+        <div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
     </Container>
   </header>
 </template>
