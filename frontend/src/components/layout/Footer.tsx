54be334ab738c9e0cbf705960b96e8b162eb5d1c
DavePaurillo <paurillo.dave@gmail.com>
2025-08-23 13:03:27 +0800

    nuked the frontend. switching to vue

--- frontend/src/components/layout/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-black text-white">
-      <div className="polaris-container">
-        <h1>Footer</h1>
-      </div>
-    </footer>
-  );
-}
