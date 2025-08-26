31c75c5c173398a593588fd94321b8fa332bfe77
DavePaurillo <paurillo.dave@gmail.com>
2025-08-27 00:57:41 +0800

    ui(wigs): add goal button and the dialog when it is clicked

--- frontend/src/components/features/wigs/Wigs.vue
+++ frontend/src/components/features/wigs/Wigs.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-import { PlusCircleIcon } from '@heroicons/vue/24/outline'
 import WigItem from './components/WigItem.vue'
 import { useWigsStore } from '@/stores/wigs.store'
 import { watch } from 'vue'
 import { useAuth } from '@clerk/vue'
 import { storeToRefs } from 'pinia'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { PlusCircleIcon } from '@heroicons/vue/24/outline'
 
 const { isLoaded, isSignedIn, getToken } = useAuth()
 const wigsStore = useWigsStore()
@@ -36,21 +47,49 @@ watch(
     <div class="flex items-center justify-between gap-4">
       <h2 class="text-2xl font-bold">Wigs</h2>
       <div class="flex items-center gap-2">
-        <Button
-          class="polaris-shadow-sm bg-black hover:scale-105 hover:cursor-pointer hover:bg-black/85 focus:scale-105"
-        >
-          <PlusCircleIcon class="overflow-hidde size-6 text-white" />
-          <p>Add a wig</p>
-        </Button>
+        <Dialog>
+          <DialogTrigger as-child>
+            <Button>
+              <PlusCircleIcon class="mr-2 size-4" />
+              <p>Add new wig</p>
+            </Button>
+          </DialogTrigger>
+          <DialogContent class="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Create a Wig</DialogTitle>
+              <DialogDescription>
+                You can create a wig here. The maximum wigs you can create is 4. This will make it
+                easier to manage and focus on the most important ones.
+              </DialogDescription>
+            </DialogHeader>
+            <div class="grid gap-4 py-4">
+              <div class="grid grid-cols-4 items-center gap-4">
+                <Label for="title" class="text-right"> Title </Label>
+                <Input
+                  id="title"
+                  default-value="A goal that you must do today"
+                  class="col-span-3"
+                />
+              </div>
+              <div class="grid grid-cols-4 items-center gap-4">
+                <Label for="description" class="text-right"> Description </Label>
+                <Input
+                  id="description"
+                  default-value="something about the goal"
+                  class="col-span-3"
+                />
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit"> Save changes </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
 
     <div v-for="wig in wigs" class="mt-6 flex flex-col gap-4">
       <WigItem :key="wig.id" :wig="wig" />
-      <!-- <WigItem />
-      <WigItem />
-      <WigItem />
-      <WigItem /> -->
     </div>
   </div>
 </template>
