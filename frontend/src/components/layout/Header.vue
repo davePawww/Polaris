4018258449d80ca2bb55f92c3d62cdbd62997d8f
DavePaurillo <paurillo.dave@gmail.com>
2025-09-08 00:22:33 +0800

    misc: placeholder for Preferences and Pomodoro settings

--- frontend/src/components/layout/Header.vue
+++ frontend/src/components/layout/Header.vue
@@ -16,7 +16,31 @@ import PolarisLight from '@/assets/Polaris-light.svg'
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UserButton />
+          <UserButton>
+            <UserButton.UserProfilePage label="Preferences" url="preferences">
+              <template #labelIcon>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
+                  <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"></path>
+                </svg>
+              </template>
+              <div>
+                <h1>Help Page</h1>
+                <p>This is the custom help page</p>
+              </div>
+            </UserButton.UserProfilePage>
+
+            <UserButton.UserProfilePage label="Pomodoro Settings" url="pomodoroSettings">
+              <template #labelIcon>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor">
+                  <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z"></path>
+                </svg>
+              </template>
+              <div>
+                <h1>Help Page</h1>
+                <p>This is the custom help page</p>
+              </div>
+            </UserButton.UserProfilePage>
+          </UserButton>
         </SignedIn>
       </div>
     </div>
