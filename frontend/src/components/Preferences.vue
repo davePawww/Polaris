9205cc72f400c0e121e64cf42bc749b0fc3d2571
DavePaurillo <paurillo.dave@gmail.com>
2025-09-09 21:05:40 +0800

    styles: added enabled dark mode settings

--- frontend/src/components/Preferences.vue
+++ frontend/src/components/Preferences.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { UserButton } from '@clerk/vue'
-import { Cog8ToothIcon } from '@heroicons/vue/24/solid'
+import { Cog8ToothIcon, SunIcon, MoonIcon } from '@heroicons/vue/24/solid'
+import { Switch } from '@/components/ui/switch'
+import { Label } from '@/components/ui/label'
+
+const isDark = ref(false)
+const toggleTheme = () => {
+  isDark.value = !isDark.value
+}
 </script>
 
 <template>
@@ -9,8 +17,19 @@ import { Cog8ToothIcon } from '@heroicons/vue/24/solid'
       <Cog8ToothIcon />
     </template>
     <div>
-      <h1>Help Page</h1>
-      <p>This is the custom help page</p>
+      <h1>Preferences</h1>
+      <div class="mt-4 flex items-center gap-2">
+        <Label for="theme-switcher" class="text-xs">Enable Dark mode</Label>
+        <!-- <Switch :model-value="isDark" @update:model-value="toggleTheme"> -->
+        <Switch id="theme-switcher" :model-value="isDark" @update:model-value="toggleTheme">
+          <template #thumb>
+            <div class="flex size-4 items-center justify-center">
+              <MoonIcon class="size-3" v-if="isDark" />
+              <SunIcon class="size-3" v-else />
+            </div>
+          </template>
+        </Switch>
+      </div>
     </div>
   </UserButton.UserProfilePage>
 </template>
